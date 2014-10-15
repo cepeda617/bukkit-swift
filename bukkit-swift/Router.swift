f3a964d07de03e5cb7ff106075aa2c2bbf843a34
Peter Cepeda <cepeda617@gmail.com>
2014-10-14 21:53:56 -0400

    Using Alamofire Router to get users

---
+++ bukkit-swift/Router.swift
@@ -0,0 +1,62 @@
+//
+//  Router.swift
+//  bukkit-swift
+//
+//  Created by Peter Cepeda on 10/14/14.
+//  Copyright (c) 2014 Bukkit. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum Router: URLRequestConvertible {
+  static let baseURLString = "http://localhost:3000/api/v1"
+  static let OAuthToken: String? = "97507b3209a119d52cc289c6c1b187e2"
+  
+  case Users()
+  case CreateUser([String: AnyObject])
+  case ReadUser(String)
+  case UpdateUser(String, [String: AnyObject])
+  case DestroyUser(String)
+  
+  var method: Alamofire.Method {
+    switch self {
+    case .Users: return .GET
+    case .CreateUser: return .POST
+    case .ReadUser: return .GET
+    case .UpdateUser: return .PUT
+    case .DestroyUser: return .DELETE
+    }
+  }
+  
+  var path: String {
+    switch self {
+    case .Users: return "/users"
+    case .CreateUser: return "/users"
+    case .ReadUser(let id): return "/users/\(id)"
+    case .UpdateUser(let id, _): return "/users/\(id)"
+    case .DestroyUser(let id): return "/users/\(id)"
+    }
+  }
+  
+  // MARK: URLRequestConvertible
+  
+  var URLRequest: NSURLRequest {
+    let URL = NSURL(string: Router.baseURLString)
+      let mutableURLRequest = NSMutableURLRequest(URL: URL.URLByAppendingPathComponent(path))
+      mutableURLRequest.HTTPMethod = method.toRaw()
+      
+      if let token = Router.OAuthToken {
+        mutableURLRequest.setValue("Token token=\(token)", forHTTPHeaderField: "authorization")
+      }
+      
+      switch self {
+      case .CreateUser(let parameters):
+        return Alamofire.ParameterEncoding.JSON.encode(mutableURLRequest, parameters: parameters).0
+      case .UpdateUser(_, let parameters):
+        return Alamofire.ParameterEncoding.URL.encode(mutableURLRequest, parameters: parameters).0
+      default:
+        return mutableURLRequest
+      }
+  }
+}
--- bukkit-swift/User.swift
+++ bukkit-swift/User.swift
@@ -11,54 +11,31 @@ import Alamofire
 import SwiftyJSON
 
 class UserManager {
-  class func all() {
-    var users = Array<User>()
-    Alamofire.request(.GET, "http://localhost:3000/api/v1/users?token=97507b3209a119d52cc289c6c1b187e2")
-      .responseJSON { (request, response, json, error) in
-        let json = JSON(json!)
-        for (index: String, subJson: JSON) in json {
-          users.append(User(json: subJson))
-        }
+
+  func fetch() {
+    Alamofire.request(Router.Users()).responseJSON { (request, response, json, error) in
+      var users = [User]()
+      for (index: String, subJson: JSON) in JSON(json!) {
+        users.append(User(json: subJson))
       }
+      // render users
     }
-    
-//    BukkitAPI().manager.request(.GET, "http://localhost:3000/api/v1/users")
-//      .responseJSON { (request, response, JSON, error) in
-//        println(request)
-//        println(response)
-//        println(JSON)
-//        println(error)
-//    }
-//    var users = Users()
-//    API().request.GET("/users", parameters: nil, success: { (response: HTTPResponse) in
-//      if response.responseObject != nil {
-////        let dict = response.responseObject as Dictionary<String,AnyObject>
-////        println("example of the JSON key: \(dict["users"])")
-//        println("print the whole response: \(JSONDecoder(response.responseObject!))")
-//        users = Users(JSONDecoder(response.responseObject!))
-//      }
-//    }, failure: { (error: NSError) in
-//      println("error: \(error)")
-//    })
-//    println(users.users)
-//    return users
-//  }
+  }
+  
+  func get(id: String) {
+    Alamofire.request(Router.ReadUser(id)).responseJSON { (request, response, json, error) in
+      var user = User(json: JSON(json!))
+      // render user
+    }
+  }
 }
 
 class User {
-  var id: Int?
-  var name: String?
-  var email: String?
-  var avatar_url: String?
-  var url: String?
-  
-  init(id: Int, name: String, email: String, avatar_url: String, url: String) {
-    self.id = id
-    self.name = name
-    self.email = email
-    self.avatar_url = avatar_url
-    self.url = url
-  }
+  var id: Int
+  var name: String
+  var email: String
+  var avatar_url: String
+  var url: String
   
   init(json: JSON) {
     self.id = json["id"].intValue
@@ -68,37 +45,3 @@ class User {
     self.url = json["url"].stringValue
   }
 }
-
-//struct Users : JSONJoy {
-//  var users: Array<User>!
-//  
-//  init() {}
-//  
-//  init(_ decoder: JSONDecoder) {
-////    decoder.array(&users)
-//    if let array = decoder["users"].array {
-//      users = Array<User>()
-//      for user in array {
-//        users.append(User(user))
-//      }
-//    }
-//  }
-//}
-//
-//struct User : JSONJoy {
-//  var id: Int?
-//  var name: String?
-//  var email: String?
-//  var avatar_url: String?
-//  var url: String?
-//  
-//  init() {}
-//  
-//  init(_ decoder: JSONDecoder) {
-//    id = decoder["id"].integer
-//    name = decoder["name"].string
-//    email = decoder["email"].string
-//    avatar_url = decoder["avatar_url"].string
-//    url = decoder["url"].string
-//  }
-//}
